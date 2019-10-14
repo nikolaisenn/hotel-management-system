ae2e6d7ae74caa54cc5e1d9c79196612e5bcecd5
Nikolay Dimitrov <nikolay.dimitrov@student.cs.man.ac.uk>
2019-10-14 13:46:10 +0100

    Running NodeJS

--- app/app.js
+++ app/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+//var cons = require('consolidate');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,8 +11,9 @@ var usersRouter = require('./routes/users');
 var app = express();
 
 // view engine setup
+//app.engine('html', cons.swig)
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -39,3 +41,4 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
+
--- app/routes/index.js
+++ app/routes/index.js
@@ -3,7 +3,26 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index');
+});
+
+router.get('/index.html', function(req, res, next) {
+  res.render('index');
+});
+
+/* GET accommodation page. */
+router.get('/accommodation.html', function(req, res, next) {
+  res.render('accommodation');
+});
+
+/* GET register page. */
+router.get('/register.html', function(req, res, next) {
+  res.render('register');
+});
+
+/* GET login page. */
+router.get('/login.html', function(req, res, next) {
+  res.render('login');
 });
 
 module.exports = router;
