2b9d89d9175c8232dd5a2734c11048005070b21e
Nikolay Dimitrov <nikolay.dimitrov@student.cs.man.ac.uk>
2019-10-23 10:40:50 +0100

    Hashed password

--- app/controllers/htmlController.js
+++ app/controllers/htmlController.js
@@ -2,6 +2,8 @@
 
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
+var bcrypt = require('bcryptjs');
+
 
 var urlencodedParser = bodyParser.urlencoded({ extended: false });
 
@@ -21,9 +23,23 @@ module.exports = function(app) {
             if(err) throw err;
             console.log("Connected!");
             
+            // Hash password
+            // Generate a salt
+            var salt = bcrypt.genSaltSync(10);
+            // Hash the password with salt
+            var hash = bcrypt.hashSync("my password", salt);
+
+            // var hashedPassword;
+            // bcrypt.genSalt(10, function(err, salt) {
+            //     bcrypt.hash(req.body.password, salt, function(err, hash) {
+            //         if(err) throw err;
+            //         hashedPassword = hash;
+            //     })
+            // })
+            // console.log(hashedPassword);
             var sql = "INSERT INTO `client` (`firstname`, `lastname`, `email`, `username`, `password`, `address`, `client_id`) " + 
                     "VALUES ('"+req.body.first_name+"', '"+req.body.last_name+"', '"+req.body.email+"', " +
-                            " '"+req.body.username+"', '"+req.body.password+"', NULL, NULL)";
+                            " '"+req.body.username+"', '"+hash+"', NULL, NULL)";
             console.log(sql);
 
             con.query(sql, function(err, result) {
