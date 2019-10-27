c01ca4cffd9097eeee3359ab8680238b0bd580c0
Nikolay Dimitrov <nikolay.dimitrov@student.cs.man.ac.uk>
2019-10-27 13:57:18 +0000

    Added indexController

--- app/api/routes/index.js
+++ app/api/routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index');
-});
+// Require controllers
+var indexController = require('../../controllers/indexController');
+
+/* GET index page */
+router.get('/', indexController.indexPage);
 
 module.exports = router;
---
+++ app/controllers/indexController.js
@@ -0,0 +1,11 @@
+'use strict';
+
+var bodyParser = require('body-parser');
+var mysql = require('mysql');
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+/* GET - Index page */
+module.exports.indexPage = function(req, res) {
+    res.render('index');
+};
