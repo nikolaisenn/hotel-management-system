ed8ab2f75d8b8957b7ee1e8f9c80bb0161e0e35a
Nikolay Dimitrov <nikolay.dimitrov@student.cs.man.ac.uk>
2019-12-16 16:43:15 +0000

    commit

--- app/controllers/roomsController.js
+++ app/controllers/roomsController.js
@@ -35,7 +35,9 @@ module.exports.availabilityPage = function (req, res) {
 	// Obtain values from input forms 
 	var { date_checkin, date_checkout, dropdown_adult, dropdown_children } = req.body;
 	date_checkin = new Date(date_checkin)
+	date_checkin.setHours(12)
 	date_checkout = new Date(date_checkout)
+	date_checkout.setHours(12)
 	dropdown_adult = parseInt(dropdown_adult)
 	dropdown_children = parseInt(dropdown_children)
 
@@ -59,22 +61,32 @@ module.exports.availabilityPage = function (req, res) {
 				var i;
 				var j;
 				var length = rooms.length;
+				var startingLength = rooms.length;
 				var removedItemsCount = 0;
+				rooms.sort(function(a, b) {
+					return a.id - b.id
+				});
+				console.log(rooms)
 				// Check if the room reservations are conflicting with the desired dates by the user
 				// If that is the case, cut all room instances with this id from the rooms array
-				for (i = 0; i < length; i++) {
-					if ((rooms[i-removedItemsCount]['reservations.date_in'] >= date_checkin && rooms[i-removedItemsCount]['reservations.date_in'] <= date_checkout) 
-						|| (rooms[i-removedItemsCount]['reservations.date_out'] >= date_checkin && rooms[i-removedItemsCount]['reservations.date_out'] <= date_checkout)) {
-						console.log("Thе room with id " + rooms[i-removedItemsCount].id + " should not appear in results");
-						var conflictRoom = rooms[i-removedItemsCount]
-						// Reset rooms length and removed items count before cutting more room instances
-						length = rooms.length
-						removedItemsCount = 0;
-						for (j = 0; j < length; j++) {
-							if (rooms[j-removedItemsCount].id == conflictRoom.id) {
-								rooms.splice(j - removedItemsCount, 1)
-								removedItemsCount++
+				// Check for undefined because the outer for loop uses the initial length, which shortens during execution
+				for (i = 0; i < startingLength; i++) {
+					if (rooms[i-removedItemsCount] != undefined) {
+						if ((rooms[i-removedItemsCount]['reservations.date_in'] > date_checkin && rooms[i-removedItemsCount]['reservations.date_in'] < date_checkout) 
+							|| (rooms[i-removedItemsCount]['reservations.date_out'] > date_checkin && rooms[i-removedItemsCount]['reservations.date_out'] < date_checkout)) {
+							console.log("Thе room with id " + rooms[i-removedItemsCount].id + " should not appear in results");
+							var conflictRoom = rooms[i-removedItemsCount]
+							
+							// Reset removed items count before cutting room instances
+							removedItemsCount = 0;
+							for (j = 0; j < length; j++) {
+								if (rooms[j-removedItemsCount].id == conflictRoom.id) {
+									rooms.splice(j - removedItemsCount, 1)
+									removedItemsCount++
+								}
 							}
+							// Reset length after cutting room instances
+							length = rooms.length
 						}
 					}
 				}
@@ -112,25 +124,24 @@ module.exports.availabilityPage = function (req, res) {
 /* POST booking */
 module.exports.booking = function (req, res) {
 	var { fromDate, toDate, roomid} = req.body;
-	console.log(fromDate);
-
 	var checkin = new Date(fromDate);
 	var checkout = new Date(toDate);
 	var room_id = parseInt(roomid);
-	console.log(checkin);
-	console.log(checkout);
-	console.log(room_id);
-	const Op = Sequelize.Op;
+	console.log(checkin)
+	console.log(checkout)
+	console.log(room_id)
+
+	// Reservations start and end at 12pm
+	checkin.setHours(12);
+	checkout.setHours(12);
 
 	const newReservation = Reservation.build({
 		date_in: checkin,
 		date_out: checkout,
 		room_id: room_id
 	})
-
-	console.log("New reservation details...");
-	console.log(newReservation);
-
+	// console.log("New reservation details...");
+	// console.log(newReservation);
 	newReservation.save()
 		.then(function (user) {
 			// Create a flash message
